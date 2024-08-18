62c737df45280c0df2b1a9eda2b7b8bf0c492ef6
bforbiggy <andyhecsp@gmail.com>
2024-08-17 20:33:59 -0400

    Transparent

---
+++ src/routes/results/+page.svelte
@@ -0,0 +1,1 @@
+<p>Got nothing lol!</p>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import questions from "$lib/Questions.js";
+	import { setContext } from "svelte";
 
 	let index = 0;
-	let stats = {
+	let results = {
 		E: 0,
 		I: 0,
 		S: 0,
@@ -12,6 +13,11 @@
 		J: 0,
 		P: 0,
 	};
+
+	// When done, store results and go to result page
+	$: index >= questions.length &&
+		setContext("results", results) &&
+		goto("/results");
 </script>
 
 <div class="flex flex-col items-center min-h-screen p-16">
@@ -26,7 +32,7 @@
 			<button
 				class="px-4 py-2 font-bold text-gray-800 bg-gray-300 rounded hover:bg-gray-400"
 				on:click={() => {
-					stats[choice.stat]++;
+					results[choice.stat]++;
 					index++;
 				}}
 			>
