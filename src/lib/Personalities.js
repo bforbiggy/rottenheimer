0ad046523f3b392df2d9d29d19ec350d8775de20
bforbiggy <andyhecsp@gmail.com>
2024-08-23 17:52:42 -0400

    ENTP JONKLA

---
+++ src/lib/Personalities.js
@@ -0,0 +1,21 @@
+import Jonkler from "$lib/assets/jonkler.png";
+
+export default {
+	ENTP: {
+		image: Jonkler,
+		name: "The Jonkler",
+		description: "The jonkes on YOU!!!!",
+		pros: [
+			{
+				short: "Rizzy",
+				long: "You are rizzy wit it so all the gyatts fawn all over you!"
+
+			}],
+		cons: [
+			{
+				short: "Copium",
+				long: "Can't handle going sisyphus mode so you need to huff copium sometimes"
+			}
+		]
+	}
+}
--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,11 +1,22 @@
 <script>
 	import { page } from "$app/stores";
+	import Personalities from "$lib/Personalities.js";
 
 	let searchParams = $page.url.searchParams;
 	let personality = searchParams.get("personality");
+	let representation = Personalities[personality];
 </script>
 
-<div class="flex flex-col items-center min-h-screen p-16">
-	<p class="text-3xl">Personality Type</p>
-	<p class="text-2xl">{personality}</p>
+<div class="flex flex-col max-w-[100vw] items-center min-h-screen p-4 lg:p-16">
+	<p class="text-5xl font-bold">
+		{representation?.name || personality || "The Undefined"}
+	</p>
+	<p class="text-xl opacity-60">
+		{representation?.description || "Trying to break the system are we?	"}
+	</p>
+	<img
+		class="size-[600px] max-w-full h-auto"
+		src={representation.image}
+		alt=""
+	/>
 </div>
