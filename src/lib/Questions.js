765b3fc79e18df46e6657b1a6a82a825b0d2ac78
bforbiggy <andyhecsp@gmail.com>
2024-08-29 16:03:47 -0400

    More questions

--- src/lib/Questions.js
+++ src/lib/Questions.js
@@ -1,54 +1,169 @@
+const QUESTION_FORMAT = {
+	question: "",
+	choices: [
+		{
+			choice: "",
+			stat: "",
+		},
+	]
+};
+
 export default [
 	{
-		"question": "Are you an alpha rizzler or a beta mouth breather?",
-		"choices": [
+		question: "Are you an alpha rizzler or a beta mouth breather?",
+		choices: [
 			{
-				"choice": "Alpha Rizzler",
-				"stat": "E",
+				choice: "Alpha Rizzler",
+				stat: "E",
 			},
 			{
-				"choice": "Beta Mouth Breather",
-				"stat": "I",
+				choice: "Beta Mouth Breather",
+				stat: "I",
 			}
 		]
 	},
 	{
-		"question": "You are shown being caught in 4k so you're skibidi cooked. Do you try to uno reverse card them on twitter or accept it as a #girlboss #queen moment?",
-		"choices": [
+		question: "You are shown being caught in 4k so you're skibidi cooked. Do you try to uno reverse card them on twitter or accept it as a #girlboss #queen moment?",
+		choices: [
 			{
-				"choice": "Uno reverse card on twitter",
-				"stat": "S",
+				choice: "Uno reverse card on twitter",
+				stat: "S",
 			},
 			{
-				"choice": "Girlboss #slay #queen",
-				"stat": "N",
+				choice: "Girlboss #slay #queen",
+				stat: "N",
 			}
 		]
 	},
 	{
-		"question": "A blud walks up to you and starts yapping. Do you let your aura speak or do you powerscale them first?",
-		"choices": [
+		question: "A blud walks up to you and starts yapping. Do you let your aura speak or do you powerscale them first?",
+		choices: [
 			{
-				"choice": "Powerscale their ass first",
-				"stat": "T",
+				choice: "Powerscale their ass first",
+				stat: "T",
 			},
 			{
-				"choice": "Aura tech the bastard",
-				"stat": "F",
+				choice: "Aura tech the bastard",
+				stat: "F",
 			}
 		]
 	},
 	{
-		"question": "When you are gooning, do you lock in and edgemaxx or do you \"Nah, imma do my own thing.\"",
-		"choices": [
+		question: "When you are gooning, do you lock in and edgemaxx or do you \"Nah, imma do my own thing.\"",
+		choices: [
 			{
-				"choice": "Edgemaxx",
-				"stat": "J",
+				choice: "Edgemaxx",
+				stat: "J",
 			},
 			{
-				"choice": "\"Nah, imma do my own thing.\"",
-				"stat": "P",
+				choice: "\"Nah, imma do my own thing.\"",
+				stat: "P",
 			}
+		]
+	},
+	{
+		question: "You're invited to hangout at Tiktok Rizz Party. Do you go or return to your aesthetically pleasing goon cave alone?",
+		choices: [
+			{
+				choice: "Tiktok Rizz Party",
+				stat: "E",
+			},
+			{
+				choice: "Aesthetically pleasing goon cave",
+				stat: "I",
+			}
+		]
+	},
+	{
+		question: "When wagecucking, do you think of out of pocket ideas or do you focus on securing that bag?",
+		choices: [
+			{
+				choice: "Securing that bag",
+				stat: "S",
+			},
+			{
+				choice: "Out of pocket ideas",
+				stat: "N",
+			}
+		]
+	},
+	{
+		question: "The tea is that your brainrot is becoming terminal. Do you defensive yap or do you sleep on them haters? ",
+		choices: [
+			{
+				choice: "Defensive Yap",
+				stat: "T",
+			},
+			{
+				choice: "Sleep on them haters",
+				stat: "F",
+			}
+		]
+	},
+	{
+		question: "Are your plans periodt or are you a pick me girl?",
+		choices: [
+			{
+				choice: "Periodt",
+				stat: "J",
+			},
+			{
+				choice: "Pick me girl!",
+				stat: "P",
+			}
+		]
+	},
+	{
+		question: "You're stuck in ohio, which is such an oof moment. Is it time to get litty or do you iPad kid?",
+		choices: [
+			{
+				choice: "Get litty",
+				stat: "E",
+			},
+			{
+				choice: "iPad kid",
+				stat: "I",
+			}
+		]
+	},
+	{
+		question: "Member when you were goated? Did shit slap fo sho or were you drippy wit da gucci?",
+		choices: [
+			{
+				choice: "Drippy wit da gucci",
+				stat: "S",
+			},
+			{
+				choice: "Shit slaps fo sho",
+				stat: "N",
+			},
+		]
+	},
+	{
+		question: "A hater is tweaking right now. Do you sksksksk them or do you make sure you're valid first?",
+		choices: [
+			{
+				choice: "Sksksksk attack",
+				stat: "T",
+			},
+			{
+				choice: "I'm #valid",
+				stat: "F",
+			},
+		]
+	},
+	{
+		question: "Do you sleep on due dates until you're cooked or do you ratio the quest giver?",
+		choices: [
+			{
+				choice: "Sleep on due dates",
+				stat: "J",
+			},
+			{
+				choice: "Ratio the quest giver",
+				stat: "P",
+			},
 		]
 	}
 ]
+
