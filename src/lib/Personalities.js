8c47b2a7b11d673a88f0b4c73063f209b7513675
bforbiggy <andyhecsp@gmail.com>
2024-08-29 16:12:36 -0400

    Trait aliases

--- src/lib/Personalities.js
+++ src/lib/Personalities.js
@@ -4,24 +4,60 @@ import Himdoriya from "$lib/assets/himdoriya.png";
 import Mewer from "$lib/assets/mewer.png";
 import Oppenheimer from "$lib/assets/oppenheimer.png";
 
+const STRENGTHS = {
+	bold: {
+		short: "Alpha",
+		long: "Ain't no playing no games straight up +1 million aura"
+	},
+	charismatic: {
+		short: "Rizzy",
+		long: "You are rizzy wit it so all the gyatts fawn all over you!"
+	},
+	altruistic: {
+		short: "Nakama",
+		long: "YOU ARE MY FRIEND ^-^"
+	},
+	honest: {
+		short: "REAL",
+		long: "\"A real ass mf who don't spit no cap\""
+	},
+	analytical: {
+		short: "Third Eye",
+		long: "THE VOICES TELL ME EVERYTHING"
+	}
+};
+
+const WEAKNESSES = {
+	impulsive: {
+		short: "Gooner",
+		long: "You lack the discipline to edge"
+	},
+	intolerant: {
+		short: "Copium",
+		long: "Can't handle going sisyphus mode so you need to huff copium sometimes"
+	},
+	unrealistic: {
+		short: "Delulu",
+		long: "Can't get a grip on reality, delusion is required"
+	},
+	skeptical: {
+		short: "Sussy",
+		long: "\"Can't trust nobody, not even yourself.\"",
+	},
+	overthinker: {
+		short: "Mind's eye",
+		long: "THE VOICES TELL ME TOO MUCH"
+	}
+};
+
 export default {
 	ESTJ: undefined,
 	ESTP: {
 		image: Mewer,
 		name: "Giga Mewer",
 		blurb: "\"I do mew, how could you tell?\"",
-		pros: [
-			{
-				short: "Alpha",
-				long: "Ain't no playing no games straight up +1 million aura"
-			}
-		],
-		cons: [
-			{
-				short: "Gooner",
-				long: "You lack the discipline to edge"
-			}
-		]
+		pros: [STRENGTHS.bold],
+		cons: [WEAKNESSES.impulsive]
 	},
 	ESFJ: undefined,
 	ESFP: undefined,
@@ -30,39 +66,15 @@ export default {
 		image: Jonkler,
 		name: "The Jonkler",
 		blurb: "\"The jonkes on YOU!!!!\"",
-		pros: [
-			{
-				short: "Rizzy",
-				long: "You are rizzy wit it so all the gyatts fawn all over you!"
-			}
-		],
-		cons: [
-			{
-				short: "Copium",
-				long: "Can't handle going sisyphus mode so you need to huff copium sometimes"
-			}
-		]
+		pros: [STRENGTHS.charismatic],
+		cons: [WEAKNESSES.intolerant]
 	},
 	ENFJ: {
 		image: Mighty,
 		name: "Mr. Might",
 		blurb: "\"This country is saved. Why? Because I am here.\"",
-		pros: [
-			{
-				short: "Rizzy",
-				long: "You are rizzy wit it so all the gyatts fawn all over you!"
-			},
-			{
-				short: "Nakama",
-				long: "YOU ARE MY FRIEND ^-^"
-			}
-		],
-		cons: [
-			{
-				short: "Delulu",
-				long: "Can't get a grip on reality, delusion is required"
-			}
-		]
+		pros: [STRENGTHS.charismatic, STRENGTHS.altruistic],
+		cons: [WEAKNESSES.unrealistic]
 	},
 	ENFP: undefined,
 	ISTJ: undefined,
@@ -70,18 +82,8 @@ export default {
 		image: Himdoriya,
 		name: "Himdoriya",
 		blurb: "\"I'm not him...\"",
-		pros: [
-			{
-				short: "REAL",
-				long: "\"A real ass mf who don't spit no cap\""
-			}
-		],
-		cons: [
-			{
-				short: "Sussy",
-				long: "\"Like an imposter, you can't trust your gut for no reason\"",
-			}
-		]
+		pros: [STRENGTHS.honest],
+		cons: [WEAKNESSES.skeptical]
 	},
 	ISFJ: undefined,
 	ISFP: undefined,
@@ -90,18 +92,8 @@ export default {
 		image: Oppenheimer,
 		name: "Oppenheimer",
 		blurb: "\"My le bomb.... le killed people?\"",
-		pros: [
-			{
-				short: "Third Eye",
-				long: "THE VOICES TELL ME EVERYTHING"
-			}
-		],
-		cons: [
-			{
-				short: "Mind's eye",
-				long: "THE VOICES TELL ME TOO MUCH"
-			}
-		]
+		pros: [STRENGTHS.analytical],
+		cons: [WEAKNESSES.overthinker]
 	},
 	INFJ: undefined,
 	INFP: undefined
